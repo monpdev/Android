47c4b2f313a8e05005c54754b1d5e1db52ec4bc2
monpdev <35469743+monpdev@users.noreply.github.com>
2021-06-17 11:28:53 +0100

    Create forLoop.java

---
+++ Android2021C/forLoop.java
@@ -0,0 +1,16 @@
+package monp.example.learnjava;
+
+import java.sql.SQLOutput;
+
+public class MyClass {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10 ; i++) {
+            if(i%2 == 0)
+            System.out.println(i + " is a multiple of 2");
+            
+        }
+
+    }
+
+}
