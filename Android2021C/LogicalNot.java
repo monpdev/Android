c612b961d930ad5e0febdd0d713c01400179ca3a
monpdev <35469743+monpdev@users.noreply.github.com>
2021-06-17 10:45:27 +0100

    Create LogicalNot.java

---
+++ Android2021C/LogicalNot.java
@@ -0,0 +1,18 @@
+package monp.example.learnjava;
+
+import java.sql.SQLOutput;
+
+public class MyClass {
+    public static void main(String[] args) {
+
+        boolean isFunny = true;
+
+        if (!isFunny) {
+            System.out.println("Not funny");
+        } else {
+            System.out.println("Funny");
+        }
+
+    }
+
+}
