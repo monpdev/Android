ae412f9b7da6f2629ec21cfa36778e9ba1d94c0f
monpdev <35469743+monpdev@users.noreply.github.com>
2021-06-18 09:53:57 +0100

    Create ifelse.java

---
+++ Android2021C/ifelse.java
@@ -0,0 +1,22 @@
+package monp.example.learnjava;
+
+import java.sql.SQLOutput;
+
+public class MyClass {
+    public static void main(String[] args) {
+
+        char grade = 'C';
+
+        if(grade == 'A'){
+            System.out.println("A is for Awesome!");
+        }else if(grade == 'B'){
+            System.out.println("B is for Beautifully done!");
+        }else if(grade == 'C'){
+            System.out.println("C is for Careless.");
+        }else if(grade == 'F'){
+            System.out.println("F is for Future Unknown.");
+        }
+
+    }
+
+}
