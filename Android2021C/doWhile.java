e802890561be3ed2a3a9ac5742ac3dbb5e03a3d2
monpdev <35469743+monpdev@users.noreply.github.com>
2021-06-20 17:30:27 +0100

    Create doWhile.java

---
+++ Android2021C/doWhile.java
@@ -0,0 +1,19 @@
+package monp.example.learnjava;
+
+import java.sql.SQLOutput;
+
+public class MyClass {
+    public static void main(String[] args) {
+
+        int limit = 10;
+        int counter = 1;
+
+        do {
+            System.out.println("We keep counting...");
+            counter++;
+        }while(counter < limit);
+
+
+    }
+
+}
