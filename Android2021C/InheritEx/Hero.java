bd9eea58e9531d29119df07121151e794c59acf0
monpdev <35469743+monpdev@users.noreply.github.com>
2021-07-05 13:30:55 +0100

    Create Hero.java

---
+++ Android2021C/InheritEx/Hero.java
@@ -0,0 +1,11 @@
+package monp.example.learnjava;
+
+public class Hero extends Person {
+    int power;
+
+    public Hero(String name, int age,int power){
+        super(name,age);
+        this.power = power;
+
+    }
+}
