59054b70cfdd0e879e08b364f56a44e2d3bcdb86
monpdev <35469743+monpdev@users.noreply.github.com>
2021-07-16 14:32:58 +0100

    Create showResults.java

---
+++ Android2021C/BoxOverride/showResults.java
@@ -0,0 +1,32 @@
+package monp.example.learnjava;
+
+import java.sql.SQLOutput;
+
+public class showResults {
+
+    public static void main(String[] args) {
+        
+      
+
+        Box box1 = new Box();
+        box1.setLength(7);
+        box1.setWidth(5);
+
+        System.out.println("Box area: " + box1.getLength()*box1.getWidth());
+
+       
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
