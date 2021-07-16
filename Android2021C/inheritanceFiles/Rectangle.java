1fb0d08afb8fdb63d1097acaceb0d6bb05193d8b
monpdev <35469743+monpdev@users.noreply.github.com>
2021-07-16 05:47:45 +0100

    Create Rectangle.java

---
+++ Android2021C/inheritanceFiles/Rectangle.java
@@ -0,0 +1,34 @@
+package monp.example.learnjava;
+
+public class Rectangle {
+    //instance variables/properties/fields
+    private int length;
+    private int width;
+
+    //Create a constructor
+    public Rectangle(){}
+
+    public Rectangle(int length, int width){
+        this.length = length;
+        this.width = width;
+    }
+
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+
+}
