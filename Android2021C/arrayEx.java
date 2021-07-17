2f81ed9514f482a016e28753e2e51996ca5c79a4
monpdev <35469743+monpdev@users.noreply.github.com>
2021-07-17 15:30:36 +0100

    Create arrayEx.java

---
+++ Android2021C/arrayEx.java
@@ -0,0 +1,29 @@
+package monp.example.learnjava;
+
+import java.sql.SQLOutput;
+
+public class arrayEx {
+
+    public static void main(String[] args) {
+
+        int[] myArray = {1, 33, 4, 12, 89, 98, 90, 12, 43, 1234};
+        String[] names = {"James","Anthony","Rodrigo","Niraj"};
+
+        for(int i = 0; i < myArray.length; i++){
+            System.out.println("Items : " + myArray[i]);
+        }
+
+        //System.out.println(names[2]);
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
