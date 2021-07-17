af0d725c07de51ff550fc04ca734f7a69d3e85b8
monpdev <35469743+monpdev@users.noreply.github.com>
2021-07-17 15:44:58 +0100

    Create AverageGpa.java

---
+++ Android2021C/AverageGpa.java
@@ -0,0 +1,18 @@
+package monp.example.learnjava;
+
+public class AverageGpa {
+
+    public double getAverageGpa(){
+        double[] gpas = {4.0, 3.4, 2.5, 1.0, 3.0};
+        double gpaSum = 0.0;
+        double avgGpa = 0.0;
+
+        //Calculate the average gpa
+        for(int i = 0; i < gpas.length; i++){
+            gpaSum += gpas[i];
+            agvGpa = gpaSum/gps.length;
+        }
+
+        return avgGpa;
+    }
+}
