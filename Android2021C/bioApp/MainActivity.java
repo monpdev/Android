f29602b89e0e623fe32d81b0cb3a2a1adcfdc9ce
monpdev <35469743+monpdev@users.noreply.github.com>
2021-07-27 21:43:28 +0100

    Create MainActivity.java

---
+++ Android2021C/bioApp/MainActivity.java
@@ -0,0 +1,35 @@
+package monp.example.bio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText enterHobbies;
+    private TextView hobbies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        enterHobbies = findViewById(R.id.enter_hobbies);
+        hobbies = findViewById(R.id.hobbies_text);
+    }
+
+    public void addHobbies(View view) {
+        hobbies.setText(String.format("Hobbies: %s", enterHobbies.getText().toString().trim()));
+        hobbies.setVisibility(View.VISIBLE);
+
+        //hide keyboard
+        //InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)
+        // inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+
+    }
+
+}
