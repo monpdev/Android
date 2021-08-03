8b427f02c47bccd814631be524cb79281bb02333
monpdev <35469743+monpdev@users.noreply.github.com>
2021-08-03 11:46:00 +0100

    Create TrueFalse.java

---
+++ AndroidO/Quizzler/TrueFalse.java
@@ -0,0 +1,30 @@
+package com.londonappbrewery.quizzler;
+
+import android.print.PrinterId;
+
+public class TrueFalse {
+
+    private int mQuestionID;
+    private boolean mAnswer;
+
+    public TrueFalse(int questionResourceID, boolean trueOrFalse){
+        mQuestionID = questionResourceID;
+        mAnswer = trueOrFalse;
+    }
+
+    public int getQuestionID() {
+        return mQuestionID;
+    }
+
+    public void setQuestionID(int questionID) {
+        mQuestionID = questionID;
+    }
+
+    public boolean isAnswer() {
+        return mAnswer;
+    }
+
+    public void setAnswer(boolean answer) {
+        mAnswer = answer;
+    }
+}
