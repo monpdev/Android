8d63f430832bde76b3c46806a1be8e29566c1d4b
monpdev <35469743+monpdev@users.noreply.github.com>
2021-08-20 13:21:30 +0100

    Create CarSimulator.java

---
+++ AndroidO/Cars/CarSimulator.java
@@ -0,0 +1,20 @@
+package monp.example.javapractice;
+
+public class CarSimulator {
+    public static void main(String[] args) {
+        System.out.println("Launching car simulator.");
+
+        Car myToyota = new Car("Blue", "Wood");
+
+        Car myHonda = new Car();
+
+        myHonda.drive();
+
+        SelfDrivingCar autoBot = new SelfDrivingCar("Yellow","Plastic");
+        autoBot.drive();
+        System.out.println("autoBot colour is:" +autoBot.colour);
+        System.out.println("autoBot interior is: " +autoBot.mInterior);
+
+
+    }
+}
